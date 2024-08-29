3bafe23bc103b7c2c3e66cf0d1d837a6eeea21de
Jansiya jahan <jansiyajahan8@gmail.com>
2024-08-29 22:06:45 +0530

    add the new User Controller

--- backend/Controllers/userContoller.ts
+++ backend/Controllers/userContoller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
+import User from "../Models/userModel";
 
-
-
-
- export const testController=(req: Request, res: Response)=>{
-    res.status(200).send("Hello, World!");
- }
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
