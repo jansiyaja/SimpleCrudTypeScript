267d6307fa3bc9be98e35b88d1a0800ff392c6d8
Jansiya jahan <jansiyajahan8@gmail.com>
2024-08-30 22:41:23 +0530

    added the login controller and refresh controller to handle the refresh and access token

--- backend/Controllers/userContoller.ts
+++ backend/Controllers/userContoller.ts
@@ -1,11 +1,81 @@
-import { Request, Response } from "express";
-import {dumyData} from '../Models/demoUsers'; // Import your mock data
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import userModel from '../Models/userModel';
 
-export const getUsers = async (req: Request, res: Response) => {
+import { generateAccessToken, generateRefreshToken } from '../Jwt/jwt';
+
+
+interface IRegisterRequest extends Request {
+  body: {
+      username: string;
+      email: string;
+      password: string;
+  };
+}
+
+
+interface ILoginRequest extends Request {
+  body: {
+      email: string;
+      password: string;
+  };
+}
+
+
+export const register = async (req: IRegisterRequest, res: Response) => {
+  const { username, email, password } = req.body;
+
   try {
-   
+      const existUser = await userModel.findOne({ email });
+      if (existUser) {
+          return res.status(400).json({ message: 'User already exists' });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const newUser = new userModel({
+          name: username,
+          email,
+          password: hashedPassword,
+      });
+      await newUser.save();
+
+      res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    console.error('Error fetching users:', error); // Log the error
-    res.status(500).json({ message: "Server Error" });
+      console.error(`Error: ${(error as Error).message}`);
+      res.status(500).json({ message: 'Server error' }); 
+  }
+};
+
+export const login = async (req: ILoginRequest, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+      const user = await userModel.findOne({ email });
+      if (!user) {
+          return res.status(400).json({ message: 'User does not exist' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+      }
+
+      const accessToken = generateAccessToken(user._id.toString());
+      const refreshToken = generateRefreshToken(user._id.toString());
+
+      res.status(200).json({
+          message: 'User logged in successfully',
+          accessToken,
+          refreshToken,
+          data: {
+              name: user.name,
+              email: user.email,
+          },
+      });
+  } catch (error) {
+      console.error(`Error: ${(error as Error).message}`);
+      res.status(500).json({ message: 'Server error' });
   }
 };
