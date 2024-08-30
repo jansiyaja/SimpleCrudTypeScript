82c8dcb520b3832cffbf6302d6b1bf40ce33841f
Jansiya jahan <jansiyajahan8@gmail.com>
2024-08-30 22:18:51 +0530

    import the feature of axios and managing the refresh and acess Token

--- frontend/src/assets/Components/Auth/Login.tsx
+++ frontend/src/assets/Components/Auth/Login.tsx
@@ -1,17 +1,79 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import axiosInstance from '../axiosInstance';
+import {useUser} from '../userContext';
+
+
+ interface ILogin{
+    email:string;
+    password:string
+ }
 
 const Login: React.FC = () => {
+    
+    const [error, setError] = useState<string>("");
+    const[login,setLogin]=useState<ILogin>({email:'',password:''})
+
+
+   const navigate = useNavigate();
+   const { setUser } = useUser();
+
+   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+   
+    
+        setLogin({
+           
+            
+            ...login,
+            [e.target.name]: e.target.value
+        });
+    };
+   
+
+    const handleLogin = async (e:React.FocusEvent<HTMLFormElement>) => {
+        e.preventDefault()
+         try {
+            const response = await axiosInstance.post('/users/login', login);
+            
+            if (response.status === 200) {
+                console.log("respose",response.data)
+
+                const accessToken=response.data.accessToken
+                const refreshToken=response.data.refreshToken
+                const userData={
+                    name:response.data.data.name,
+                    email:response.data.data.email
+
+                }
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('refreshToken', refreshToken);
+               console.log(userData);
+             
+                setUser(userData);
+                navigate('/');
+              
+            }
+            console.log("respose not  get")
+        } catch (error) {
+            setError('Invalid credentials. Please try again.');
+            console.error('There was an error logging in!', error);
+        }
+    };
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-red-500 to-white">
             <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
                 <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
                         <input 
+                             name='email'
+                            value={login.email}
                             type="email" 
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                             placeholder="Enter your email"
+                            onChange={handleChange}
                         />
                     </div>
                     <div className="mb-6">
@@ -20,6 +82,9 @@ const Login: React.FC = () => {
                             type="password" 
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
                             placeholder="Enter your password"
+                            name='password'
+                            value={login.password}
+                            onChange={handleChange}
                         />
                     </div>
                     <div className="flex items-center justify-between">
@@ -29,6 +94,7 @@ const Login: React.FC = () => {
                         >
                             Login
                         </button>
+                        {error && <p className=' text-red-500'>{error}</p>}
                     </div>
                 </form>
             </div>
